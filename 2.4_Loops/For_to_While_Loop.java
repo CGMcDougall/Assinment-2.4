b83e2bcf14fbc910db1f36b75c83a79237b6f059
EatPieGuys <56049883+EatPieGuys@users.noreply.github.com>
2019-10-03 12:25:36 -0700

    Add files via upload

---
+++ 2.4_Loops/For_to_While_Loop.java
@@ -0,0 +1,18 @@
+
+public class For_to_While_Loop
+{
+    public static void main (String[] args){
+    //for (i = 100; i >= 10; i--)
+//{
+//System.out.println("i is: " + i);
+//}
+
+int i = 100;
+while(i >= 10){
+  System.out.println("i is: " + i);  
+ i--;   
+}
+
+
+}
+}
---
+++ 2.4_Loops/Output_of_Loop.java
@@ -0,0 +1,18 @@
+
+public class Output_of_Loop
+{
+       public static void main (String[] args){
+           
+           
+    for(int i = 1; i <= 10; i++){
+for (int j = 1 ; j <= i; j++)
+{
+System.out.print(j + " ");
+}
+System.out.println("");
+}
+
+//Output is a number Pyramid looking thing
+
+    }
+}
